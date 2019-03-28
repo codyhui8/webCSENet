c0858972668380d704f3eb960eebc94b5727082c
codyhui8 <codyhui8@gmail.com>
2019-03-28 15:13:59 -0500

    Fixed random comma on page

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(
     <Router>
-        <App />,
+        <App />
     </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
